2b776a343a60009de893775d8aefb75fd08857d6
Leahnakaima <lnakaima@hks.harvard.edu>
2020-01-14 15:44:14 -0500

    added name, read and wrote their codes

---
+++ readwrite.py
@@ -0,0 +1,9 @@
+#reads name.txt into a variable my_name and then
+
+with open('name.txt') as f:
+    my_name = f.read()
+
+#writes a new file named hello.txt with the contents Hello, my name is <my_name>.
+with open('hello.txt', 'w') as f:
+    f.write('hello, my name is ' + my_name)
+    f.write('\n')
