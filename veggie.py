f9fcd04d141d6652a4e6d370cd3d6f06ceb5865d
Leahnakaima <lnakaima@hks.harvard.edu>
2020-01-14 16:23:30 -0500

    added veggies, colors, and creates a two-column table

---
+++ veggie.py
@@ -0,0 +1,23 @@
+#By Leah Nakaima and Nick Anway
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+
+#Loops through each vegetable
+#In the loop, write the name of each vegetable and the color into a CSV called vegetables.csv
+
+import csv
+
+with open('vegetables.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerow(['name', 'color'])
+	for x in vegetables:
+		name = x["name"]
+		color = x["color"]
+		writer.writerow([name, color])
